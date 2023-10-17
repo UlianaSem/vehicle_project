a16885cb79f967bbed5dac9f0924c03ee47f9fd0
Uliana Semyonova <i@ulianasemyonova.ru>
2023-10-17 11:33:02 +0500

    Add .env

--- config/settings.py
+++ config/settings.py
@@ -9,20 +9,24 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pvc*e^ra@esfp)bn4izyspadd$vnorfwjtz1@jd8&vln7#g*%('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -83,10 +87,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vehicle_project',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'NAME': os.getenv("DATABASES_NAME"),
+        'USER': os.getenv("DATABASES_USER"),
+        'PASSWORD': os.getenv("DATABASES_PASSWORD"),
+        'HOST': os.getenv("DATABASES_HOST"),
     }
 }
 
@@ -155,5 +159,5 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-EXCHANGE_RATE_API_KEY = "w5VosbkOtkrksSPd2QdrUQ95UxNtpjQ0"
-EXCHANGE_RATE_URL = "https://api.apilayer.com/exchangerates_data/convert"
+EXCHANGE_RATE_API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
+EXCHANGE_RATE_URL = os.getenv("EXCHANGE_RATE_URL")
