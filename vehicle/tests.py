0ce19201209d7e7bab90a8ef509dba36d14c27c5
Uliana Semyonova <i@ulianasemyonova.ru>
2023-10-17 11:17:34 +0500

    Add tests

--- vehicle/tests.py
+++ vehicle/tests.py
@@ -1,3 +1,80 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+from vehicle.models import Moto, Milage
+
+"""Задача
+
+    Необходимо обложить тестами один эндпоинт создания сущности
+    Посчитать покрытие тестами.
+
+    проверить покрытие тестами."""
+
+
+class MotoTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="test@test.ru", password='test', is_staff=True, is_superuser=True)
+        self.client.force_authenticate(user=self.user)
+
+        self.moto = Moto.objects.create(name="Harley-Davidson Ultra Limited", description='test')
+        self.milage = Milage.objects.create(moto=self.moto, distance=1000, year="2021")
+        self.data = {
+            "name": "Honda CBF 1000",
+            "description": "test",
+            "milage": [
+                {
+                    "distance": 2000,
+                    "year": "2020"
+                }
+            ],
+        }
+
+    def test_create(self):
+        response = self.client.post(
+            reverse('vehicle:moto-create'),
+            data=self.data,
+        )
+        print(response.data)
+
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEquals(
+            response.json(),
+            {
+                "id": 2,
+                "milage": [],
+                "name": "Honda CBF 1000",
+                "description": "test",
+                "owner": self.user.pk,
+                "price": None
+            }
+        )
+
+    def test_detail(self):
+        response = self.client.get(
+            reverse('vehicle:moto-detail', args=[self.moto.pk])
+        )
+
+        self.assertEquals(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+        self.assertEquals(
+            response.json(),
+            {
+                "id": self.moto.pk,
+                "name": self.moto.name,
+                "description": self.moto.description,
+                "owner": self.moto.owner,
+                "last_milage": 1000,
+                'price': None,
+                'usd_price': None
+            }
+        )
